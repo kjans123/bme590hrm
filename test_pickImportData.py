4f3d4e3406ae0b05220fb830110a64fcf2cb998a
kjans123 <kj123@duke.edu>
2018-02-21 13:51:50 -0500

    changed test file for single picker to ref relative file path

--- test_pickImportData.py
+++ test_pickImportData.py
@@ -2,7 +2,7 @@ def test_pickImportData():
     from pickImportData import PickImportData
     import pandas as pd
     testClass = PickImportData()
-    testClass.FilePath = "/home/kj123/bme590hrm/test_data1.csv"
+    testClass.FilePath = "test_data1.csv"
     testClass.ImportFile()
     assert type(testClass.outPutArray) == pd.core.frame.DataFrame
 
